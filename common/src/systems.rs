aa3c615e3c41f3950c8979339c518c2cb5456183
taktiks2 <deathproof.lee@gmail.com>
2024-12-04 21:39:16 +0900

    delete no need function

--- common/src/systems.rs
+++ common/src/systems.rs
@@ -1,7 +1,5 @@
 use bevy::prelude::*;
 
-use crate::resources::GameAssets;
-
 pub fn despawn_screen<T: Component>(mut commands: Commands, to_despawn: Query<Entity, With<T>>) {
     for entity in &to_despawn {
         commands.entity(entity).despawn_recursive();
@@ -10,11 +8,4 @@ pub fn despawn_screen<T: Component>(mut commands: Commands, to_despawn: Query<En
 
 pub fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2d);
-}
-
-pub fn setup_game_assets(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.insert_resource(GameAssets {
-        font_bold: asset_server.load("fonts/NotoSansJP-Bold.ttf"),
-        font_regular: asset_server.load("fonts/NotoSansJP-Regular.ttf"),
-    })
 }
